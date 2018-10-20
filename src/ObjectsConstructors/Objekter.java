46deefa7611fd52af13ea6e144d8960b3ceb3882
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-10-20 22:04:01 +0200

    Constructor tilføjet

---
+++ src/ObjectsConstructors/Objekter.java
@@ -0,0 +1,4 @@
+package ObjectsConstructors;
+
+public class Objekter {
+}
---
+++ src/ObjectsConstructors/PoelseConstructor.java
@@ -0,0 +1,27 @@
+package ObjectsConstructors;
+
+import java.awt.*;
+
+import static java.awt.Color.black;
+
+public class PoelseConstructor {
+
+    public static void main(String[] args) {
+
+        // en constructor laver objekter ud fra de givne parametre i constructoren,
+        // her laver vi en pølse constructor som kan vise pølsens farve længde og type
+
+        PoelseConstructor poelse1 = new PoelseConstructor(black, 5, "frankfurter");
+
+        }
+
+    public PoelseConstructor(Color color, int lenght, String type){
+
+        Color poelsefarve = color;
+        int poelselenght = lenght;
+        String poelsetype = type;
+
+        System.out.println("New poelse object created");
+
+    }
+}
