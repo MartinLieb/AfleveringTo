d4aa48912560d7befff44fbd1665e2935ead9da5
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-10-20 22:49:23 +0200

    print tilføjet

---
+++ src/print/Printf.java
@@ -0,0 +1,16 @@
+package print;
+
+public class Printf {
+
+    public static void main(String[] args) {
+
+        // printf er en anden måde at printe ting i konsolen hvor vi kan få formater med
+        // her printer vi en sætning med 2 formater
+
+        int myInt = 10; // her defineres en variabel
+        String myString = "damer der er vild med mig"; // her defineres en variabel
+
+        System.out.printf("Jeg har %d %s", myInt, myString); // her printer vi en kæmpe løgn
+
+    }
+}
---
+++ src/print/Sout.java
@@ -0,0 +1,21 @@
+package print;
+
+public class Sout {
+
+    public static void main(String[] args) {
+
+        // med sout (system.out.println) kan vi udskrive ting i konsolen
+        // her kan man være lidt kreativ for at vise hvad man kan bruge det til
+        // her printer vi en fin lille smiley :)
+
+        System.out.println("" +
+                "       .-'''''-.\n" +
+                "    .'          '.\n" +
+                "   /   (o)   (o)   \\\n" +
+                "  |                 |\n" +
+                "  |  \\           /  |\n" +
+                "   \\  '.       .'  /\n" +
+                "    '.   '---'  .'\n" +
+                "       '-. _ .-'");
+    }
+}
